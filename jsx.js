810a62ff6f5921c0aa39ab3808c4e3271b7b1040
조민수 <chominsu10@gmail.com>
2021-06-15 15:22:31 +0900

    [ADD] 어거지로 사진넣기

--- jsx.js
+++ jsx.js
@@ -18,6 +18,9 @@ const renderElement = Node => {
     return document.createTextNode(Node);
   }
   const el = document.createElement(Node.type);
+  if (Node.props.src) {
+    el.src = Node.props.src;
+  }
   // 재귀 해서 자식의 자식까지 엘리먼트를 만들어준다
   Node.children.map(renderElement).forEach(element => {
     //forEach함수로 각각의 자식을 그 부모의 자식으로 위치시킨다
@@ -32,16 +35,32 @@ const render = (DOM, root) => {
   root.appendChild(renderElement(DOM));
 };
 
+const background = [
+  "https://cdn.discordapp.com/attachments/676942969710051360/853799693238534144/ab0b164fd8c516bc.gif",
+  "https://w.namu.la/s/32d07783a93f968dd7fadae25f254c6e1135dc3d09285bb48785cc798096aa1667c53945035397ffa3d28a233788d7f032d218dd2af5e8b703bccd7a11d5c34cd9a6d3aa13831d227b495673a9db9297fa2852af59e834a4811e3c75bd5dffd8",
+  "https://img.hankyung.com/photo/201808/03.17543429.1.jpg",
+  "https://img.insight.co.kr/static/2019/01/31/700/18nd52ajs5z4750u7p6f.jpg",
+  "http://ojsfile.ohmynews.com/STD_IMG_FILE/2020/0130/IE002597642_STD.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr3FDg9Q7JRnV2SGsvXhRJyafXFWZKatrUHg&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSs52rnezs7eGzT8hxJkEHhtHivFzOh03XPYz1i_xOvIZaDR2bMmyU0IHYXMQlYNhTdg_I&usqp=CAU",
+  "https://image.musinsa.com/mfile_s01/2020/02/07/f99687dd719c4e8bc6a39e946c3d9ef7114955.jpg",
+  "https://pbs.twimg.com/media/EQJGuF9U0AEB3QG.jpg",
+  "https://media.discordapp.net/attachments/676942969710051360/853822425614254100/0662f3d46bf89a438289c8d56615c29567af2395.gif",
+  "https://media.discordapp.net/attachments/676942969710051360/853828865939210260/2d3d928f40bebeafb24185adab9206e5e790ba4a_re_1621692482933.gif",
+];
+
 function App() {
   return (
-    <div>
-      <div>
-        <h1>안녕</h1>
+    <div src>
+      <div src>
+        <h1 src>안녕</h1>
       </div>
-      <ul>
-        <li>이것이</li>
-        <li>바로</li>
-        <li>JSX</li>
+      <ul src>
+        <li src>이것이</li>
+        <li src>바로</li>
+        <li src>JSX</li>
+        <button src>아년ㅇ</button>
+        <img src={background[0]}>aa</img>
       </ul>
     </div>
   );
